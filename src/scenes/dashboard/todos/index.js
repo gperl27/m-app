24c3eb842b164cd0ec31165f9bbf02a4c33455c9
Greg P <gperlman27@gmail.com>
2017-12-14 19:02:05 -0500

    abstract todolist & todo into separate files

--- src/scenes/dashboard/todos/index.js
+++ src/scenes/dashboard/todos/index.js
@@ -1,24 +1,44 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+
+import TodoList from './TodoList';
+
 class TodosContainer extends Component {
+    renderTodoLists() {
+        return this.props.user.todolists.map(todolist => {
+            return <TodoList 
+                key={todolist.id} 
+                todolist={todolist} 
+                handleTitleChange={this.handleTitleChange}
+            />
+        })
+    }
+
+    handleTitleChange(e) {
+        console.log(e.title, 'input change');
+    }
+
     render() {
         return (
             <div>
                 <button type="button" className="btn btn-outline-success">New List</button>
-                <div className="card">
-                    <div className="card-body">
-                        <h4 className="card-title">Groceries</h4>
-                        <ul className="list-group list-group-flush">
-                            <li className="list-group-item">Cras justo odio</li>
-                            <li className="list-group-item">Dapibus ac facilisis in</li>
-                            <li className="list-group-item">Vestibulum at eros</li>
-                        </ul>
-                        <button type="button" className="btn btn-outline-danger">Delete</button>
-                    </div>
-                </div>
+                {this.renderTodoLists()}
             </div>
         );
     }
 }
 
-export default TodosContainer;
+const mapStateToProps = state => ({
+    user: state.user.user,
+    selectedDate: state.user.selectedDate,
+})
+
+const mapDispatchToProps = dispatch => bindActionCreators({
+
+}, dispatch)
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(TodosContainer)
